f5333168d2dfacf453185e2af54a591e42c8cb73
LepingLi0810 <107237943+LepingLi0810@users.noreply.github.com>
2022-10-09 20:45:41 -0500

    Update ccbench.c

--- src/ccbench.c
+++ src/ccbench.c
@@ -457,42 +457,22 @@ void *run_test(void *arg) {
 	  }
 	case CAS: /* 12 */
 	  {
-	    switch (task->id)
-	      {
-	      default:
-		sum += cas_0_eventually(cache_line, reps);
-		break;
-	      }
+	    sum += cas_0_eventually(cache_line, reps);
 	    break;
 	  }
 	case FAI: /* 13 */
 	  {
-	    switch (task->id)
-	      {
-	      default:
-		sum += fai(cache_line, reps);
-		break;
-	      }
+	    sum += fai(cache_line, reps);
 	    break;
 	  }
 	case TAS:		/* 14 */
 	  {
-	    switch (task->id)
-	      {
-	      default:
-		sum += tas(cache_line, reps);
-		break;
-	      }
+            sum += tas(cache_line, reps);
 	    break;
 	  }
 	case SWAP: /* 15 */
 	  {
-	    switch (task->id)
-	      {
-	      default:
-	        sum += swap(cache_line, reps);
-		break;
-	      }
+	    sum += swap(cache_line, reps);
 	    break;
 	  }
 	case CAS_ON_MODIFIED: /* 16 */
